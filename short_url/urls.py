4619e0dbf05baf4e4a14771f3d09a1d413d22ca3
abhishek rana <ar10695@gmail.com>
2018-04-05 21:18:12 +0530

    retrieve data from database and redirection work

--- short_url/urls.py
+++ short_url/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 app_name = "short_url"
 urlpatterns = [
     path(r'', views.index, name='index'),
+    re_path(r'(?P<code>[\w]{8})/', views.page_redirect, name='page_redirect' )
 ]
--- short_url/views.py
+++ short_url/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import string
 import random
-from short_url.models import url_mapping
+from short_url.models import url_mapping, deleted_url
 
 
 def index(request):
@@ -27,3 +27,22 @@ def index(request):
 
 def id_generator(size=8, chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
+
+
+def page_redirect(request, code):
+    if request.method == "GET":
+        print (code)
+        info = url_mapping.objects.get(short_url=code)
+        if info and info.ttl ==-1:
+            data=dict()
+            data["long_url"]=info.long_url
+            return render(request, 'short_url/page_redirect.html', data)
+        elif info and info.ttl!=-1:
+            return render(request, 'short_url/delete_redirect.html')
+        elif deleted_url.objects.get(deleted_entry=code):
+            return render(request, 'short_url/delete_redirect.html')
+        else:
+            return render(request, 'short_url/delete_redirect.html')
+
+
+
